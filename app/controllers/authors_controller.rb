6094feb5a25dde1cc8d9c946edbba3f9c90d39c1
Elijah wines <elijahwines84@yahoo.com>
2020-07-08 17:31:45 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,6 +3,11 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
-  
+
+  def posts_index
+    @author = Author.find(paeams[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
+  end
 
 end
